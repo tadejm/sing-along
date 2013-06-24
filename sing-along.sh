767683c6d3c4161576d6f3dfaf85017d008f3152
Tadej Murovec <tadej.murovec@gmail.com>
2013-06-25 00:59:14 +0200

    Add bash script for fetching.

---
+++ sing-along.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+artist=`osascript -e 'tell application "iTunes" to artist of current track as string'`;
+title=`osascript -e 'tell application "iTunes" to name of current track as string'`;
+
+SERVER="http://sing-along.herokuapp.com/lyrics"
+
+OPTS="-G -A 'Sing-along agent'"
+DATA="--data-urlencode \"artist=${artist}\" --data-urlencode \"title=${title}\""
+
+output=`eval curl $OPTS $SERVER $DATA` > /dev/null
+echo -e $output | less
